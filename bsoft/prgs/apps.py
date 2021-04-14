8a71a40190dca50ebf8ad70ab1edf3e1661236a6
Volodymyr271 <81734420+Volodymyr271@users.noreply.github.com>
2021-04-14 19:08:39 +0300

    Initial commit

---
+++ bsoft/prgs/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class PrgsConfig(AppConfig):
+    name = 'prgs'
---
+++ bsoft/prgs/views.py
@@ -0,0 +1,6 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(requst):
+    return HttpResponse('hello from box')
